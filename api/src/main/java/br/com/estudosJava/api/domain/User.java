08b619a3d6043fe23f0c2b2e17a46aae42fc22ec
lukazz54 <lucassantos95@hotmail.com.br>
2024-07-05 11:28:10 -0300

    Include User class

---
+++ api/src/main/java/br/com/estudosJava/api/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.estudosJava.api.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
