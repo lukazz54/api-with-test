e76059417f1c60ba406331824a9b0db731b687f5
lukazz54 <lucassantos95@hotmail.com.br>
2024-07-05 12:28:20 -0300

    add test profile

--- api/src/main/java/br/com/estudosJava/api/domain/User.java
+++ api/src/main/java/br/com/estudosJava/api/domain/User.java
@@ -1,9 +1,10 @@
 package br.com.estudosJava.api.domain;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @Entity
