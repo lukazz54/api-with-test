56365b12bbaa5aa74221282905b558c6f3e2c6ce
lukazz54 <lucassantos95@hotmail.com.br>
2024-07-15 09:07:17 -0300

    test for method delete with success

--- api/src/test/java/br/com/estudosJava/api/services/impl/UserServiceImplTest.java
+++ api/src/test/java/br/com/estudosJava/api/services/impl/UserServiceImplTest.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -140,7 +140,13 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteWithSuccess() {
+        when(userRepository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(userRepository).deleteById(anyInt());
+
+        service.delete(ID);
+
+        verify(userRepository, times(1)).deleteById(anyInt());
     }
 
     private void startUser() {
